38ed91035a5352243d57abd1fb3e10e8483b88a8
tonyreaves <tonyfreaves@gmail.com>
2018-06-11 11:58:20 -0700

    pushing data to Firebase, displaying most recent train

--- assets/trains.js
+++ assets/trains.js
@@ -1,50 +1,80 @@
-var database = firebase.database();
+const database = firebase.database();
 
-    // Initial Values
-    var train = "";
-    var destination = "";
-    let firstTrain = 0;
-    var frequency = "";
+// Initial Values
+let train = "";
+let destination = "";
+let firstTrain = 0;
+let frequency = 0;
 
-    // Capture Button Click
-    $("#add-user").on("click", function(event) {
-      event.preventDefault();
+// Capture Button Click
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
 
-      // Grabbed values from text boxes
-      train = $("#name-input").val().trim();
-      destination = $("#email-input").val().trim();
-      firstTrain = $("#age-input").val().trim();
-      frequency = $("#comment-input").val().trim();
+    // Grabbed values from text boxes
+    train = $("#train-name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTrain = $("#first-train-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
 
-      // Code for handling the push
-      database.ref().push({
+    const firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+
+    const currentTime = moment();
+
+    const diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+
+    const tRemainder = diffTime % frequency;
+
+    const minutesTillTrain = frequency - tRemainder;
+
+    const nextTrain = moment().add(minutesTillTrain, "minutes");
+
+    console.log(train);
+    console.log(destination);
+    console.log(firstTrain);
+    console.log("Minutes till train: " + minutesTillTrain);
+    console.log("Next train " + nextTrain)
+
+    // Code for handling the push
+    database.ref().push({
         train: train,
         destination: destination,
         firstTrain: firstTrain,
         frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+        minutesTillTrain: minutesTillTrain,
+        nextTrain: nextTrain,
 
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
-    // Firebase watcher .on("child_added"
-    database.ref().on("child_added", function(snapshot) {
-      // storing the snapshot.val() in a variable for convenience
-      var sv = snapshot.val();
+});
 
-      // Console.loging the last user's data
-      console.log(sv.train);
-      console.log(sv.destination);
-      console.log(sv.firstTrain);
-      console.log(sv.frequency);
+// Firebase watcher 
+database.ref().on("child_added", function (snapshot) {
+    // storing the snapshot.val()
+    const sv = snapshot.val();
 
-      // Change the HTML to reflect
-      $("#name-display").text(sv.name);
-      $("#email-display").text(sv.email);
-      $("#age-display").text(sv.age);
-      $("#comment-display").text(sv.comment);
+    // Console.loging the last user's data
+    console.log(sv.train);
+    console.log(sv.destination);
+    console.log(sv.firstTrain);
+    console.log(sv.frequency);
 
-      // Handle the errors
-    }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    })
+    let trainShow = [];
+    let destShow = [];
+    let freqShow = [];
+    let nextShow = [];
+    let tillShow = [];
+
+    // Change HTML 
+    $("#train-display").html(sv.train);
+    $("#destination-display").push(sv.destination);
+    $("#frequency-display").push(sv.frequency);
+    // $("#next-arrival-display").push(sv.minutesTillTrain);
+    // $("#minutes-away-display").push(sv.nextTrain);
+
+
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+})
